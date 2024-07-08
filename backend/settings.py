a5c125ebec2878db0cd4053575cf8563bfd0420f
Achem Habib Nomaer <achemnomaer57@gmail.com>
2024-07-08 14:06:47 +0600

    setup csrf trusted origins

--- backend/settings.py
+++ backend/settings.py
@@ -16,6 +16,8 @@ DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
 
+CSRF_TRUSTED_ORIGINS = os.getenv('CSRF_TRUSTED_ORIGINS', '').split(',')
+
 # Application definition
 
 INSTALLED_APPS = [
